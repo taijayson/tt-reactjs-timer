a3bbf780e8c4d17549d6928c5a00bb445a54af84
taijayson <71400805+taijayson@users.noreply.github.com>
2021-07-08 18:24:23 +0300

    comments for task added

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,18 @@
 import styles from "./App.module.css";
 import Counter from "../components/Counter/Counter";
 
+// let a = 5;
+// let b = 7;
+// [a, b] = [b, a];
+// console.log(a);
+// console.log(b);
+// if ((a = 5)) {
+//   a = 7;
+//   b = 5;
+//   console.log(a);
+//   console.log(b);
+// }
+
 export default function App() {
   return (
     <div className={styles.app}>
