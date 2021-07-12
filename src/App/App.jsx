063eefefc573f0b0509f5fce42b9be5699931da7
taijayson <71400805+taijayson@users.noreply.github.com>
2021-07-12 10:19:17 +0300

    task examples added

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,17 +1,39 @@
 import styles from "./App.module.css";
 import Counter from "../components/Counter/Counter";
 
+// // TASK //
+
 // let a = 5;
 // let b = 7;
+// // NEED a=7, b=5
+
+// // 1 //
 // [a, b] = [b, a];
 // console.log(a);
 // console.log(b);
+
+// // 2 //
 // if ((a = 5)) {
 //   a = 7;
 //   b = 5;
 //   console.log(a);
 //   console.log(b);
 // }
+
+// // 3 //
+// let tmp = a;
+// a = b;
+// b = tmp;
+// console.log(a);
+// console.log(b);
+
+// // 4 //
+////// works only with natural numbers from 2 to 32 ///////
+// a = a ^ b;
+// b = a ^ b;
+// a = a ^ b;
+// console.log(a);
+// console.log(b);
 
 export default function App() {
   return (
